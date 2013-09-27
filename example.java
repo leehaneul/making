9e9ad8a740a39d311b6e933f824b10e12a83ce3c
leehaneul <leehaneul90@naver.com>
2013-09-28 00:11:31 +0900

    example_haneul
    example

---
+++ example.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+public class Example1 {
+
+	public static void main(String[] args) {
+
+		Scanner a =  new Scanner(System.in);
+		
+		System.out.println("나는 자연인이다~!");
+		System.out.println("니가 자연인이냐?" + a.nextLine() + "녀석아?");
+	}
+
+}
